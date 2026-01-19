6f1a4b61285fe8bda030f082443bfb55314e67d1
Dipesh <dipeshchourasiya12@gmail.com>
2026-01-19 14:33:04 +0530

    wrote the logic of PersonGroovy service including validations

--- script/org/moqui/party/CreatePersonCustom.groovy
+++ script/org/moqui/party/CreatePersonCustom.groovy
@@ -1,10 +1,40 @@
-// we will validate required parameters
-if (!partyId) {
+// validate required parameters
+if (!context.partyId) {
     ec.message.addError("partyId is required")
 }
-if (!firstName) {
+if (!context.firstName) {
     ec.message.addError("firstName is required")
 }
-if (!lastName) {
+if (!context.lastName) {
     ec.message.addError("lastName is required")
 }
+
+// if validation errors exist, by this it stop execution
+if (ec.message.hasError()) return
+
+// check if Party exists
+def party = ec.entity.find("party.Party")
+        .condition("partyId", context.partyId)
+        .one()
+
+if (!party) {
+    ec.message.addError("Party with partyId ${partyId} does not exist")
+    return
+}
+
+// from docs
+//def tutorial = ec.entity.makeValue("tutorial.Tutorial")
+//tutorial.setFields(context, true, null, null)
+//if (!tutorial.tutorialId) tutorial.setSequencedIdPrimary()
+//tutorial.create()
+
+// prepare data for Person creation
+// it will make empty entity object
+def person = ec.entity.makeValue("party.Person")
+person.setFields(context, true, null, null)
+
+//  Create Person
+person.create()
+
+// return response
+responseMessage = "Person ${person.firstName} ${person.lastName} created successfully!"
